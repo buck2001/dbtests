f99907cffa0fe3eb605f098c3fce7693032237a1
Peter Naylor <peter.naylor@dxc.com>
2019-01-11 13:38:01 +0000

    Additions for Java server deployments

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,7 @@ import com.example.demo.constants.AppConstants;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = { MongoAutoConfiguration.class }) // gets us over exceptions during boot for MongoDB
 @RestController
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	@RequestMapping(AppConstants.HEALTH_CHECK)
 	public String home() {
 		return AppConstants.IM_ALIVE;
